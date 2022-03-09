3a8d816467c125e4312e91a2f4dc5140344b5183
marymele <se.meron.melese@gmail.com>
2022-03-09 04:31:22 +0300

    added loader and additional ajustments

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const quoteText=document.getElementById('quote');
 const authorText=document.getElementById('author');
 const twittweBtn=document.getElementById('twitter');
 const newQuoteBtn=document.getElementById('new-quote');
+const loader=document.getElementById('loader');
+
+
+//Show Loader
+function loading() {
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+//Hide Loader
+function complete(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+    
+}
 
 //Show New Quote
 // function newQuote(){
@@ -17,6 +32,7 @@ const newQuoteBtn=document.getElementById('new-quote');
 // }
 // Get Quotes From API
 async function getQuotes(){
+    loading();
     const apiUrl='https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -29,17 +45,32 @@ async function getQuotes(){
         }else{
             authorText.innerText=quote.author;
         }
-        
+        //Reduce font size for long quotes
+        if(quote.text.length>100){
+            quoteText.classList.add('long-quote')
+        }else{
+            quoteText.classList.remove('long-quote')
+        }
         quoteText.innerText=quote.text;
-
+        complete();
 
     } catch(error){
         //Catch Error Here
     }
 }
+//Tweet Quote
+function tweetQuote() {
+    const quote= quoteText.innerText;
+    const author= authorText.innerText;
+    const twitterUrl= `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
 
-
+    window.open(twitterUrl, '_blank');
+}
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuotes);
+twittweBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
+//loading();
  
